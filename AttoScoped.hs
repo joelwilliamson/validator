4b9a4785d014c10a2443022ded4b2b5f5ec863e2
Joel Williamson <joel.s.williamson@gmail.com>
2015-05-28 23:39:31 -0400

    Faster block parser

--- AttoScoped.hs
+++ AttoScoped.hs
@@ -21,7 +21,7 @@ import Scoped (Atom(..),Block)
 
 import Data.Char(isDigit,ord)
 import Control.Applicative((<|>))
-import Data.Attoparsec.Text(Parser,char,double,isEndOfLine,many',parseOnly,satisfy,takeTill,takeWhile,takeWhile1)
+import Data.Attoparsec.Text(Parser,char,double,isEndOfLine,many',parseOnly,peekChar',satisfy,skipWhile,takeTill,takeWhile,takeWhile1)
 import Text.Parsec.Pos(SourcePos,incSourceLine)
 import Data.Text(all)
 import Data.Monoid((<>))
@@ -74,28 +74,31 @@ value = do
   _ ← lift $ char '='
   _ ← sep
   r ← rhs
+  _ ← sep
   return $ Node l r $ Just pos
   
 rhs = block <|> (singleton <$> literal)
 
-spawnBlock :: StatefulParser [Block]
-spawnBlock = do
+-- This is a parser for blocks that never backtracks
+block :: StatefulParser [Block]
+block = do
   _ ← lift $ char '{'
+  pos ← get
   _ ← sep
   l ← literal
   _ ← sep
-  r ← literal
-  _ ← sep
-  _ ← lift $ char '}'
-  return [Node l (singleton r) Nothing]
-
-regularBlock :: StatefulParser [Block]
-regularBlock = do
-  _ ← lift $ char '{'
-  _ ← sep
-  elems ← many' value
-  _ ← lift $ char '}'
-  return elems
-
-block :: StatefulParser [Block]
-block = spawnBlock <|> regularBlock
+  ch ← lift $ peekChar'
+  if ch /= '='
+    then (do
+             r ← lift $ Number <$> double
+             _ ← sep
+             _ ← lift $ char '}'
+             return [Node l (singleton r) $ Just pos]
+         )
+    else (do
+             _ ← lift $ char '='
+             _ ← sep
+             r ← rhs
+             _ ← sep
+             (Node l r (Just pos) :) <$> many' value <* (sep *> lift (char '}'))
+         )
