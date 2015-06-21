6dfc931f5d14ae26afefa1863f88641e2aab992c
Joel Williamson <joel.s.williamson@gmail.com>
2015-06-21 11:23:28 -0400

    Added Haddocks

--- Localisation.hs
+++ Localisation.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UnicodeSyntax #-}
--- Parse the csv files used for localisation. Each localisation entry consists
--- of a key followed by several entries, separated by ';'. A 'x' marks eol
-
+-- | Parse the csv files used for localisation. Each localisation entry consists
+-- of a key followed by several entries, separated by ';'. A 'x' marks eol. Due
+-- to Paradox's very relaxed parsing, the trailing 'x' is not actually needed.
 module Localisation ( Entry(..), localisationFile) where
 
 import Data.Text as T(Text,pack)
@@ -13,6 +13,8 @@ import Control.Applicative(many)
 import Data.Attoparsec.Text as A(char,eitherP,endOfLine,isEndOfLine,isHorizontalSpace,many1,parseOnly,satisfy,sepBy',takeTill,takeWhile,takeWhile1)
 
 
+-- | An entry in a localisation file. If a key isn't set, the accompanying text
+-- will be empty.
 data Entry = Entry {
   key :: !Text
   , english :: !Text
@@ -50,6 +52,8 @@ entry = do
 
 line = eitherP (entry <* A.takeTill isEndOfLine) comment
 
+-- | Given a filename (for error messages) and the contents of the file, try to
+-- parse it as a localisation file.
 localisationFile ::FilePath → Text → Either Text [Entry]
 localisationFile file t = case parseOnly (sepBy' line (many1 endOfLine) <* many (satisfy isEndOfLine)) t of
   Left e → Left $ "Error in localisation file " <> pack file <> ": " <> pack e
