a26bc89fa72006001a79faf6e278ec7256a69383
Joel Williamson <joel.s.williamson@gmail.com>
2015-06-23 13:19:18 -0400

    Use a helper function to build tests, added more variable commands

--- Tests/Command.hs
+++ Tests/Command.hs
@@ -14,37 +14,51 @@ import Test.Tasty
 import Test.Tasty.HUnit
 
 commandUnitTests = testGroup "Command Unit Tests"
-                   [ testCase "A simple concrete command" $
-                     makeCommand "abandon_heresy = yes" @?= Right (Concrete "abandon_heresy" (BooleanValue True))
-                   , testCase "Numeric command" $
-                     makeCommand "change_diplomacy = 3" @?= Right (Concrete "change_diplomacy" (NumValue 3))
-                   , testCase "Adding a trait" $
-                     makeCommand "add_trait = diligent" @?= Right (AddTrait "diligent")
-                   , testCase "Removing a trait" $
-                     makeCommand "remove_trait = wroth" @?= Right (RemoveTrait "wroth")
-                   , testCase "Set Character Flag" $
-                     makeCommand "set_character_flag = char_flag" @?= Right (SetFlag Character "char_flag")
-                   , testCase "Clear Province Flag" $
-                     makeCommand "clr_province_flag = prov_flag" @?= Right (ClrFlag Province "prov_flag")
-                   , testCase "Set a variable" $
-                     makeCommand "set_variable = { which = a which = b }" @?= Right (VarOpVar "a" Set "b")
-                   , testCase "Add two variables" $
-                     makeCommand "change_variable = { which = a which = b }" @?= Right (VarOpVar "a" Change "b")
-                   , testCase "Subtract constant from variable" $
-                     makeCommand "subtract_variable = { which = a value = 4 }" @?= Right (VarOpLit "a" Subtract 4)
-                   , testCase "Spawn a unit" $
-                     makeCommand "spawn_unit = { province = 342 owner = THIS leader = FROM home = PREV attrition = 1 troops = { archers = { 100 100 } }}"
-                     @?= Right (SpawnUnit 342 (Just This) (Just From) (Just Prev)  Nothing (Just 1) (Troops "archers" 100 100))
-                   , testCase "Break" $
-                     makeCommand "break = yes" @?= Right Break
-                   , testCase "Random Break" $
-                     makeCommand "random = { chance = 20 modifier = { factor = 0.5 trait = sloth } modifier = { factor = 2 trait = diligent } break = yes }"
-                     @?= Right (Random 20 [Modifier 0.5 [Trait "sloth"], Modifier 2 [Trait "diligent"]] [Break])
-                   , testCase "If" $
-                     makeCommand "if = { limit = { trait = humble prestige = 5 } change_diplomacy = 2 add_trait = monk}"
-                     @?= Right (If [Trait "humble", Condition (Predicate "prestige") $ NumValue 5] [Concrete "change_diplomacy" $ NumValue 2, AddTrait "monk"])
+                   [ successTest "A simple concrete command"
+                     "abandon_heresy = yes" $ Concrete "abandon_heresy" (BooleanValue True)
+                   , successTest "Numeric command"
+                     "change_diplomacy = 3" $ Concrete "change_diplomacy" (NumValue 3)
+                   , successTest "Adding a trait"
+                     "add_trait = diligent" $ AddTrait "diligent"
+                   , successTest "Removing a trait"
+                     "remove_trait = wroth" $ RemoveTrait "wroth"
+                   , successTest "Set Character Flag"
+                     "set_character_flag = char_flag" $ SetFlag Character "char_flag"
+                   , successTest "Clear Province Flag"
+                     "clr_province_flag = prov_flag" $ ClrFlag Province "prov_flag"
+                   , successTest "Set a variable"
+                     "set_variable = { which = a which = b }" $ VarOpVar "a" Set "b"
+                   , successTest "Add two variables"
+                     "change_variable = { which = a which = b }" $ VarOpVar "a" Change "b"
+                   , successTest "Check a variable against constant"
+                     "check_variable = { which = a_variable value = 10 }"
+                     $ VarOpLit "a_variable" Check 10
+                   , successTest "Divide a variable by a constant"
+                     "divide_variable = { which = a  value = 4 } "
+                     $ VarOpLit "a" Divide 4
+                   , successTest "Check variable strict equality"
+                     "is_variable_equal = { which = a which = b }"
+                     $ VarOpVar "a" Equal "b"
+                   , successTest "Subtract constant from variable"
+                     "subtract_variable = { which = a value = 4 }" $ VarOpLit "a" Subtract 4
+                   , successTest "Spawn a unit"
+                     "spawn_unit = { province = 342 owner = THIS leader = FROM home = PREV attrition = 1 troops = { archers = { 100 100 } }}"
+                     $ SpawnUnit 342 (Just This) (Just From) (Just Prev)  Nothing (Just 1) (Troops "archers" 100 100)
+                   , successTest "Break"
+                     "break = yes"
+                     Break
+                   , successTest "Random Break"
+                     "random = { chance = 20 modifier = { factor = 0.5 trait = sloth } modifier = { factor = 2 trait = diligent } break = yes }"
+                     $ Random 20 [Modifier 0.5 [Trait "sloth"], Modifier 2 [Trait "diligent"]] [Break]
+                   , successTest "If"
+                     "if = { limit = { trait = humble prestige = 5 } change_diplomacy = 2 add_trait = monk}"
+                     $ If [Trait "humble", Condition (Predicate "prestige") $ NumValue 5] [Concrete "change_diplomacy" $ NumValue 2, AddTrait "monk"]
                    ]
-  where makeCommand :: Text -> Either Error Command
-        makeCommand s = case statefulParseOnly value (initialPos "test_data") s of
-          Left _ -> Left ("",Nothing)
-          Right s -> runMaker command s
+
+makeCommand :: Text -> Either Error Command
+makeCommand s = case statefulParseOnly value (initialPos "test_data") s of
+  Left _ -> Left ("",Nothing)
+  Right s -> runMaker command s
+
+successTest :: TestName -> Text -> Command -> TestTree
+successTest name command result = testCase name $ makeCommand command @?= Right result
