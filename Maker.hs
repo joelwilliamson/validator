4cbd2c72134186334347752eeffba38abb2b4ea5
Joel Williamson <joel.s.williamson@gmail.com>
2015-07-06 22:58:54 -0400

    Somewhat better tracing of errors through alternatives

--- Maker.hs
+++ Maker.hs
@@ -26,12 +26,23 @@ show' :: Show a ⇒ a → T.Text
 show' = T.pack . show
 
 -- | Describes how to make an @a@ from a @`Tree` Atom@.
-newtype Maker a = Maker (Tree Atom -> Either Error a)
+-- If there is an error, the integer is flags how likely it is that the error
+-- message will be useful. For instance, if a key doesn't match, we are likely
+-- to backtrack out of a wrong branch of an alternative, but if the structure
+-- is wrong, this is probably an error.
+newtype Maker a = Maker (Tree Atom -> Either (Error,Reason) a)
+
+data Reason = KeyNotFound
+            | BadConversion
+            | MissingChild
+            deriving (Eq,Ord,Show)
 
 -- | @runMaker maker t@ uses the description provided by @maker@ to construct an
 -- @a@.
 runMaker :: Maker a → Tree Atom → Either Error a
-runMaker (Maker f) = f
+runMaker (Maker f) = \t -> case f t of
+  Left (e,_) -> Left e
+  Right r -> Right r
 
 instance Functor Maker where
   fmap f (Maker a) = Maker ((f<$>) <$> a)
@@ -45,23 +56,25 @@ instance Applicative Maker where
     Left l → Left l
 
 instance Alternative Maker where
-  empty = Maker $ \Node { source } -> Left ("Empty Maker",source)
-  Maker mx <|> Maker my = Maker $ \t -> case mx t of
-    Left _ → my t
-    r → r
+  empty = Maker $ \Node { source } -> Left (("Empty Maker",source), MissingChild)
+  Maker mx <|> Maker my = Maker $ \t -> case (mx t, my t) of
+    (x@(Left (_,ix)), y@(Left (_,iy))) → if ix > iy then x else y 
+    (Right x, _) -> Right x
+    (_, Right y) -> Right y
 
 instance Monad Maker where
   return x = Maker $ \_ → Right x
   (Maker f1) >>= f = Maker $ \t → case f1 t of
     Left l → Left l
-    Right a → runMaker (f a) t
+    Right a → case f a of
+      Maker f2 -> f2 t
 
 -- | @maker \<?\> err@ behaves the same as @maker@, but if @maker@ fails, the error
 -- message is prefixed by "@err@ =>"
 infix 0 <?>
 (Maker f) <?> msg = Maker $ \t → case f t of
   r@(Right _) → r
-  Left (eMsg, source) → Left (msg <> " => " <> eMsg, source)
+  Left ((eMsg, source),prio) → Left ((msg <> " => " <> eMsg, source),prio)
 
 -- | Get the source position this tree was created from.
 position = Maker $ Right . source
@@ -69,13 +82,13 @@ position = Maker $ Right . source
 -- | @firstChild m@ applies @m@ to the first child node.
 firstChild :: Maker a → Maker a
 firstChild (Maker f) = Maker $ \t → case subForest t of
-  [] → Left ("No children: "<> show' (rootLabel t),source t)
+  [] → Left (("No children: "<> show' (rootLabel t),source t), MissingChild)
   (x:_) → f x
 -- | @secondChild m@ applies @m@ to the second child node.
 secondChild :: Maker a → Maker a
 secondChild (Maker f) = Maker $ \t → case subForest t of
-  [] → Left ("No children",source t)
-  [_] → Left ("Only one child", source t)
+  [] → Left (("No children",source t), MissingChild)
+  [_] → Left (("Only one child", source t), MissingChild)
   (_:x:_) → f x
 
 -- | @mapSubForest m@ applies @m@ to every child node.
@@ -93,14 +106,15 @@ filterSubForest p (Maker f) = Maker $ f . (\Node { subForest = s,
 -- | Fail unless there is exactly one child.
 singleChild = Maker $ \t → if length (subForest t) == 1
                            then Right ()
-                           else Left ("Not one child",source t)
+                           else Left (("Not one child",source t), MissingChild)
 
 -- | @m \@\@ key@ makes @m@ by using only a subtree with rootLabel @key@. If no such
 -- subtree exists (or if @m@ fails) the entire @`Maker`@ fails.
 infixl 5 @@
 (@@) :: Maker a → Label → Maker a
 (Maker f) @@ k = Maker $ \t → case find ((==Label k) . rootLabel) $ subForest t of
-  Nothing → Left ("No child: " <> k <> " in block: " <> T.pack (show t), source t)
+  Nothing → Left (("No child: " <> k <> " in block: " <> T.pack (show t), source t)
+                  , MissingChild)
   Just t' → f t'
 
 -- | @m \@? key@: As @\@\@@, but if no child is found, return @Nothing@.
@@ -126,18 +140,23 @@ m ~? k = firstChild m @? k
 checkKey k = Maker $ \t -> case rootLabel t of
   Label l → if l == k
             then Right l
-            else Left ("Check for "<>show' k<>" failed. Found: "<>show' (rootLabel t),source t)
-  Number _ → Left ("Check for key "<>show' k<>" failed. Found number",source t)
+            else Left (("Check for "<>show' k<>" failed. Found: "<>show' (rootLabel t)
+                        ,source t)
+                       , KeyNotFound)
+  Number _ → Left (("Check for key "<>show' k<>" failed. Found number",source t)
+                   , KeyNotFound)
 
 -- | @checkKeys keys@ succeeds if the root label is an element of keys.
 checkKeys keys = Maker $ \t → if rootLabel t `elem` map Label keys
                               then Right $ rootLabel t
-                              else Left ("Check for keys failed. Found: " <> show' (rootLabel t), source t)
+                              else Left (("Check for keys failed. Found: " <> show' (rootLabel t), source t)
+                                         , KeyNotFound)
 
 -- | @excludeKeys keys@ succeeds with the root label provided the label is not in `keys`.
 excludeKeys keys = Maker $ \t → if not $ rootLabel t `elem` map Label keys
                                 then Right $ rootLabel t
-                                else Left ("Root label excluded: " <> show' (rootLabel t), source t)
+                                else Left (("Root label excluded: " <> show' (rootLabel t), source t)
+                                           , KeyNotFound)
 
 -- | @checkValue v@ succeeds if the root label of the first child is @v@.
 checkValue = firstChild . checkKey
@@ -150,7 +169,7 @@ key = Maker $ \ t → Right $ rootLabel t
 
 -- | If the root label is a number, succeed with it.
 number = Maker $ \t → case rootLabel t of
-  Scoped.Label l → Left ("Not a number: "<> show' l, source t)
+  Scoped.Label l → Left (("Not a number: "<> show' l, source t), BadConversion)
   Scoped.Number n → Right n
 
 -- | Get the label of the tree
@@ -158,7 +177,7 @@ label :: Maker Atom → Maker Label
 label (Maker f) = Maker $ \t → case f t of
   Left l → Left l
   Right (Label l) → Right l
-  Right (Number n) → error $ "Encountered number " <> show n
+  Right (Number n) → Left (("Encountered number " <> show' n, source t), BadConversion)
 
 -- | @m \@\@\@ key@: As @\@\@@, but return make a list with every matching subTree
 infixl 5 @@@
@@ -182,29 +201,38 @@ f /@# keys = filterSubForest (not . (`elem` (Label <$> keys)) . rootLabel) $ map
 
 -- | Get the root label of the single child. If the label is a number, fail.
 fetchString = Maker $ \t → case subForest t of
-  [] → Left ("Can't take a value at " <> show' (rootLabel t),source t)
-  [Node { rootLabel = Number n}] → Left ("Expected string for value of "<> show' (rootLabel t) <>", got: "<> show' n,source t)
+  [] → Left (("Can't take a value at " <> show' (rootLabel t),source t), BadConversion)
+  [Node { rootLabel = Number n}] →
+    Left (("Expected string for value of "<> show' (rootLabel t) <>", got: "<> show' n
+           , source t)
+          , BadConversion)
   [Node { rootLabel = Label l}] → Right l
-  _ → Left ("Expected a string for value of "<> show' (rootLabel t) <> ", got a block.", source t)
+  _ → Left (("Expected a string for value of "<> show' (rootLabel t) <> ", got a block."
+             , source t)
+            , BadConversion)
 
 -- | Get an event id from the label of the single child. This can either be of
 -- the form @Number id@ or @Label $ namespace <> "." <> number@.
-fetchId = Maker $ \t → case runMaker (firstChild key) t of
-  Left e → Left e
-  Right (Label k) → case parse eventId "fetchID parse" k of
-    Left _ → Left ("Not a valid eventId: "<>k,source t)
-    Right (t,Number n) → Right (t,n)
-    Right (n,Label x) → Left ("Ill-formed eventId: "<>n<>"."<>x,source t)
-  Right (Number n) → Right ("",n)
+fetchId = Maker $ \t → case firstChild key of
+  Maker f -> case f t of
+    Left e → Left e
+    Right (Label k) → case parse eventId "fetchID parse" k of
+      Left _ → Left (("Not a valid eventId: "<>k, source t), BadConversion)
+      Right (t,Number n) → Right (t,n)
+      Right (n,Label x) → Left (("Ill-formed eventId: "<>n<>"."<>x, source t)
+                                , BadConversion)
+    Right (Number n) → Right ("",n)
 
 -- | Get a boolean from the label of the single child. The label must be one of
 -- "yes", "no", "true", "false".
-fetchBool = Maker $ \t → case runMaker (firstChild key) t of
-  Left e → Left e
-  Right (Label k) → case k of
-    "false" → Right False
-    "no" → Right False
-    "true" → Right True
-    "yes" → Right True
-    _ → Left (k <> " is not a boolean value.", source t)
-  Right (Number n) → Left ("Expected boolean, got: " <> show' n, source t)
+fetchBool = Maker $ \t → case firstChild key of
+  Maker f -> case f t of
+    Left e → Left e
+    Right (Label k) → case k of
+      "false" → Right False
+      "no" → Right False
+      "true" → Right True
+      "yes" → Right True
+      _ → Left ((k <> " is not a boolean value.", source t), BadConversion)
+    Right (Number n) → Left (("Expected boolean, got: " <> show' n, source t)
+                             , BadConversion)
